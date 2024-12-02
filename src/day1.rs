b03a648bec726f59f1457eb9202308e92f0f17cd
Zachary Dremann <dremann@gmail.com>
2024-12-02 13:45:25 -0500

    optimize for day2

--- src/day1.rs
+++ src/day1.rs
@@ -7,16 +7,33 @@ impl Day for Day1 {
     type Parsed<'a> = (Vec<u32>, Vec<u32>);
 
     fn generator(input: &str) -> Self::Parsed<'_> {
-        let (mut l, mut r): (Vec<_>, Vec<_>) = input
-            .lines()
-            .map(|line| {
-                let (l, r) = line.split_once(' ').unwrap();
-                (
-                    l.trim().parse::<u32>().unwrap(),
-                    r.trim().parse::<u32>().unwrap(),
-                )
-            })
-            .unzip();
+        let lines = input.bytes().filter(|&c| c == b'\n').count();
+        let mut l = Vec::with_capacity(lines);
+        let mut r = Vec::with_capacity(lines);
+        let mut current_val = 0;
+        let mut has_val = true;
+        for b in input.bytes() {
+            match b {
+                b' ' => {
+                    if has_val {
+                        l.push(current_val);
+                        has_val = false;
+                        current_val = 0;
+                    }
+                }
+                b'\n' => {
+                    r.push(current_val);
+                    current_val = 0;
+                    has_val = true;
+                }
+                digit => {
+                    debug_assert!(digit.is_ascii_digit());
+                    current_val = current_val * 10 + (digit - b'0') as u32;
+                }
+            }
+        }
+        debug_assert_eq!(l.len(), r.len());
+        debug_assert_eq!(l.len(), lines);
         l.sort_unstable();
         r.sort_unstable();
         (l, r)
@@ -36,7 +53,7 @@ impl Day for Day1 {
         let mut remaining_r = &r[..];
         let mut similarity = 0;
         for l in l.iter().copied() {
-            // Faster to just linear search rather than binary search
+            // Faster to just linear search rather than binary search for AOC input sizes
             let start = remaining_r
                 .iter()
                 .position(|&r| r >= l)
--- src/day2.rs
+++ src/day2.rs
@@ -1,11 +1,14 @@
 use crate::Day;
+use arrayvec::ArrayVec;
 use std::fmt::Display;
 
 pub struct Day2;
 
-type Int = u32;
+type Int = u8;
+const MAX_VALUES_PER_REPORT: usize = 16;
+type Report = ArrayVec<Int, MAX_VALUES_PER_REPORT>;
 
-fn is_safe_report(report: &[u32]) -> bool {
+fn is_safe_report(report: &[Int]) -> bool {
     let mut windows = report.windows(2);
     let Some(&[a, b]) = windows.next() else {
         return false;
@@ -27,17 +30,30 @@ fn is_safe_report(report: &[u32]) -> bool {
 }
 
 impl Day for Day2 {
-    type Parsed<'a> = Vec<Vec<Int>>;
+    type Parsed<'a> = Vec<Report>;
 
     fn generator(input: &str) -> Self::Parsed<'_> {
-        input
-            .lines()
-            .map(|line| {
-                line.split_whitespace()
-                    .map(|n| n.parse().unwrap())
-                    .collect()
-            })
-            .collect()
+        let mut dst = Vec::with_capacity(2000);
+        let mut current = ArrayVec::new();
+        let mut current_val = 0;
+        for b in input.bytes() {
+            match b {
+                b' ' => {
+                    current.push(current_val);
+                    current_val = 0;
+                }
+                b'\n' => {
+                    current.push(current_val);
+                    dst.push(current);
+                    current = ArrayVec::new();
+                    current_val = 0;
+                }
+                digit => {
+                    current_val = current_val * 10 + (digit - b'0') as Int;
+                }
+            }
+        }
+        dst
     }
 
     fn part1(input: Self::Parsed<'_>) -> impl Display {
