833cf6e56cb144096dde5513fb135091afb12046
Zachary Dremann <dremann@gmail.com>
2024-12-03 06:40:03 -0500

    Update day2 for speed

--- src/day2.rs
+++ src/day2.rs
@@ -1,6 +1,7 @@
 use crate::Day;
 use arrayvec::ArrayVec;
 use std::fmt::Display;
+use std::ops::RangeBounds;
 
 pub struct Day2;
 
@@ -9,24 +10,29 @@ const MAX_VALUES_PER_REPORT: usize = 16;
 type Report = ArrayVec<Int, MAX_VALUES_PER_REPORT>;
 
 fn is_safe_report(report: &[Int]) -> bool {
-    let mut windows = report.windows(2);
-    let Some(&[a, b]) = windows.next() else {
-        return false;
-    };
-    if !(1..4).contains(&a.abs_diff(b)) {
+    let (&[x, y], rest) = report.split_first_chunk().unwrap();
+    let increasing = y > x;
+    if !(1..4).contains(&x.abs_diff(y)) {
         return false;
     }
-    let increasing = b > a;
-    for win in windows {
-        let (a, b) = (win[0], win[1]);
-        if (b > a) != increasing {
-            return false;
+    first_unsafety(y, increasing, rest).is_none()
+}
+
+fn first_unsafety(prev_value: Int, increasing: bool, remaining_report: &[Int]) -> Option<usize> {
+    let range = 1..4;
+    let mut last = prev_value;
+    for (i, &v) in remaining_report.iter().enumerate() {
+        let diff = if increasing {
+            v.wrapping_sub(last)
+        } else {
+            last.wrapping_sub(v)
+        };
+        if !range.contains(&diff) {
+            return Some(i);
         }
-        if !(1..4).contains(&a.abs_diff(b)) {
-            return false;
-        }
+        last = v;
     }
-    true
+    None
 }
 
 impl Day for Day2 {
@@ -67,13 +73,55 @@ impl Day for Day2 {
         input
             .iter()
             .filter(|&report| {
-                if is_safe_report(report) {
-                    return true;
+                let (&[x, y], rest) = report.split_first_chunk().unwrap();
+                if !(1..4).contains(&x.abs_diff(y)) {
+                    let (&z, rest) = rest.split_first().unwrap();
+                    // try omitting x:
+                    {
+                        let increasing = z > y;
+                        if (1..4).contains(&y.abs_diff(z))
+                            && first_unsafety(z, increasing, rest).is_none()
+                        {
+                            return true;
+                        }
+                    }
+                    // try omitting y
+                    {
+                        let increasing = z > x;
+                        if (1..4).contains(&x.abs_diff(z))
+                            && first_unsafety(z, increasing, rest).is_none()
+                        {
+                            return true;
+                        }
+                    }
+                    return false;
                 }
-                for i in 0..report.len() {
-                    let mut report = report.clone();
-                    report.remove(i);
-                    if is_safe_report(&report) {
+                let increasing = y > x;
+                let Some(i) = first_unsafety(y, increasing, rest) else {
+                    return true;
+                };
+                let i = i + 2;
+                // Try omitting the previous value
+                {
+                    let last_value = report[i - 2];
+                    let increasing = if i == 2 {
+                        report[i] > last_value
+                    } else {
+                        increasing
+                    };
+                    if first_unsafety(last_value, increasing, &report[i..]).is_none() {
+                        return true;
+                    }
+                }
+                // Try omitting the value
+                {
+                    let last_value = report[i - 1];
+                    let increasing = if i == 2 {
+                        report[i + 1] > last_value
+                    } else {
+                        increasing
+                    };
+                    if first_unsafety(last_value, increasing, &report[i + 1..]).is_none() {
                         return true;
                     }
                 }
