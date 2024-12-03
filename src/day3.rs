a03fec003cb680ca786b7dcd00ee7ea857b5aec2
Zachary Dremann <dremann@gmail.com>
2024-12-03 10:51:12 -0500

    day3 manual parsing

--- src/day3.rs
+++ src/day3.rs
@@ -1,12 +1,10 @@
 use crate::Day;
 use memchr::memmem;
 use std::fmt::Display;
-use winnow::prelude::*;
 
 pub struct Day3;
 
 type Int = u32;
-type PResult<T, E = ()> = winnow::PResult<T, E>;
 
 impl Day for Day3 {
     type Parsed<'a> = &'a str;
@@ -20,7 +18,7 @@ impl Day for Day3 {
         let mult_finder = memmem::Finder::new("mul(");
         let mut result = 0;
         for mul_idx in mult_finder.find_iter(input) {
-            if let Ok((x, y)) = parse_after_mul.parse_next(&mut &input[mul_idx + mult_finder.needle().len()..]) {
+            if let Some((x, y)) = parse_after_mul(&mut &input[mul_idx + mult_finder.needle().len()..]) {
                 result += x * y;
             }
         }
@@ -42,7 +40,7 @@ impl Day for Day3 {
             );
             while let Some(next_mul) = mult_finder.find(inner_input) {
                 inner_input = &inner_input[next_mul + mult_finder.needle().len()..];
-                if let Ok((x, y)) = parse_after_mul.parse_next(&mut inner_input) {
+                if let Some((x, y)) = parse_after_mul(&mut inner_input) {
                     result += x * y;
                 }
             }
@@ -57,12 +55,35 @@ impl Day for Day3 {
     }
 }
 
-fn parse_after_mul(input: &mut &[u8]) -> PResult<(Int, Int)> {
-    let x = winnow::ascii::dec_uint.parse_next(input)?;
-    let _ = ','.parse_next(input)?;
-    let y = winnow::ascii::dec_uint.parse_next(input)?;
-    let _ = ')'.parse_next(input)?;
-    Ok((x, y))
+fn parse_after_mul(input: &mut &[u8]) -> Option<(Int, Int)> {
+    let mut it = input.iter();
+    let mut x = 0;
+    for &b in &mut it {
+        match b {
+            b',' => break,
+            b'0'..=b'9' => x = x * 10 + (b - b'0') as Int,
+            _ => {
+                *input = it.as_slice();
+                return None;
+            }
+        }
+    }
+    let mut y = 0;
+    // Found a comma
+    for &b in &mut it {
+        match b {
+            b')' => break,
+            b'0'..=b'9' => y = y * 10 + (b - b'0') as Int,
+            _ => {
+                *input = it.as_slice();
+                return None;
+            }
+        }
+    }
+
+    // found a closing parenthesis
+    *input = it.as_slice();
+    Some((x, y))
 }
 
 crate::codspeed_def!(Day3);
